d56e6c090abd35f473cc25d0c68cf2414f612a84
Zeigfreid <various.authors@gmail.com>
2014-12-26 15:17:59 -0800

    convert pixel data to hex

---
+++ NEXTSTEPS.md
@@ -0,0 +1,43 @@
+NEXTSTEPS
+---------
+
+- PuzzleTile
+  - name: string
+  - colors: array of hex
+  - map: string (array of char)
+  - data: Uint8ClampedArray
+
+
+currently the step is the width of a tile pixel,
+but now we will increase the outer step to 5 times that
+so we can read in one PuzzleTile at a time
+
+bigstep is pixel dim times tile dim
+step is pixel dim
+
+tiles = {}
+legend = {}
+
+for y from 0 to image height in bigsteps
+    for x from 0 to image width in bigsteps
+        p = new PuzzleTile
+        map_index = indexFrom(x, y)
+
+        for j from y to y + tile_pixel_dim (20) in steps (4)
+            p.map[j] = ""
+
+            for i from x to x + tile_pixel_dim (20) in steps (4)
+                data = readPixelData(i, j)
+
+                // colors are unique
+                hex = constructHex(data)
+                p.colors[hex] = p.colors.length + 1 (a map from hex to number)
+
+                p.map[j][i] = p.colors[hex] (populate the map; javascript ith index of a string)
+
+        p.name = mix(p.map) // get a hash
+        tiles[p.name] = p // unique tile
+        legend[p.name] = unicodeFromHash(p.name)
+        map[map_index] = legend[p.name] // a unicode char
+
+// we have a legend, a map, and a tileset
--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
-var AugmentedImageData = function (ctx, x, y, w, h) {
+var AugmentedImageData = function AugmentedImageData (ctx, x, y, w, h) {
     var dataObject, CHUNK_SIZE = 4;
 
     dataObject = ctx.getImageData(x, y, w, h);
 
+    dataObject.readPixelDataHex = function readPixelDataHex (x, y) {
+        var index = x + (w * y), result;
+        index = index * CHUNK_SIZE;
+
+        result = dataObject.data[index + 0].toString(16) +
+            dataObject.data[index + 1].toString(16) +
+            dataObject.data[index + 2].toString(16);
+
+        return result
+    };
+
     // return an array containing the data for a single pixel
-    dataObject.readPixelData = function (x, y) {
+    dataObject.readPixelData = function readPixelData (x, y) {
         var index = x + (w * y);
         index = index * CHUNK_SIZE;
 
@@ -19,7 +30,11 @@ var AugmentedImageData = function (ctx, x, y, w, h) {
     return dataObject
 },
 
-ImageUpload = function (src, w, h) {
+/* an image an its context constructed from a source or from image data */
+/* @params src -- original image source
+ * @params w, h -- width height in screen pixels of original image
+ * */
+ImageContext = function ImageContext (src, w, h) {
     "use strict";
 
     var canvas, ctx, img, upload, TILE_DIM = 5;
@@ -49,39 +64,62 @@ ImageUpload = function (src, w, h) {
     upload.tdim = upload.tw;
     upload.pdim = upload.tw / TILE_DIM;
 
-    upload.getImageData = function () {
+    upload.getImageData = function getImageData () {
         var data = new AugmentedImageData(ctx, 0, 0, img.width, img.height);
 
         return data;
     };
 
-    upload.drawImage = function () {
+    upload.drawImage = function drawImage () {
         return ctx.drawImage(img, 0, 0);
     };
 
     return upload;
+},
+
+PuzzleTile = function PuzzleTile () {
+    var puzzle_tile = {};
+
+    puzzle_tile.toString = function toString () {
+        return "";
+    };
+
+    return {};
 };
 
 document.addEventListener('DOMContentLoaded', function () {
     "use strict";
 
-    var upload = new ImageUpload("assets/images/example.gif", 17, 13),
+    var upload = new ImageContext("assets/images/example.gif", 17, 13),
         image_data,canvas, ctx,
         TILE_DIM = 5, INTS_PER_CHUNK = 4,
-        tile_data_size, step, pixel_data = [], pixel_image, pw, ph, clamped_array;
+        tile_data_size, step, bigstep, pixel_data = [], pixel_image, pw, ph, clamped_array;
 
     upload.drawImage();
     image_data = upload.getImageData();
 
-    // read a whole game pixel
-    step = upload.pdim;
+    step = upload.pdim; // each pixel unit
+    bigstep = step * TILE_DIM; // each tile
 
-    // read in the colour of the top-left pixel of every PIXEL_DIM square
-    for (var y = 0; y < upload.img.height; y = y + step) {
-        for (var x = 0; x < upload.img.width; x = x + step) {
-            pixel_data = pixel_data.concat(image_data.readPixelData(x, y));
+    console.log(step, bigstep);
+
+    for (var y = 0; y < upload.img.height; y = y + bigstep) {
+        for (var x = 0; x < upload.img.width; x = x + bigstep) {
+            var p = new PuzzleTile();
+            // read in the colour of the top-left pixel of every PIXEL_DIM square
+            for (var j = 0; j < bigstep; j = j + step) {
+                for (var i = 0; i < bigstep; i = i + step) {
+                    var hex = image_data.readPixelDataHex(x + i, y + j);
+                    // make tile
+                    // pixel_data = pixel_data.concat(image_data.readPixelData(x + i, y + j));
+                }
+            }
+
+            console.log(hex);
+            console.log(p.toString());
         }
     }
+
 
     pw = upload.img.width/upload.pdim;
     ph = upload.img.height/upload.pdim;
@@ -92,6 +130,4 @@ document.addEventListener('DOMContentLoaded', function () {
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
     ctx.putImageData(pixel_image, upload.img.width, upload.img.height);
-
-    console.log(upload.pw, upload.ph, image_data.data.length/tile_data_size);
 });
