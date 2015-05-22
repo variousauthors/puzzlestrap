bbf6f6b2537f2bb83f1ea9b122afec641d8f3fc8
Zeigfreid <various.authors@gmail.com>
2015-05-21 19:12:52 -0700

    added a tabbar for choosing layers

--- content/js/file.js
+++ content/js/file.js
@@ -1,3 +1,30 @@
+function toggleActive(context, element) {
+    var current = context.getElementsByClassName("active"), i;
+
+    for (i = 0; i < current.length; i++) {
+        current[i].classList.remove("active");
+    }
+
+    element.classList.add("active");
+}
+
+function createLayer (create_layer_tab) {
+    var tabs = document.getElementById('layer-select');
+    var template = document.getElementById("layer-template").innerHTML.trim();
+    var new_layer = document.createElement('div');
+
+    new_layer.innerHTML = template;
+    new_layer = new_layer.firstChild;
+
+    tabs.insertBefore(new_layer, create_layer_tab);
+    toggleActive(tabs, new_layer);
+}
+
+function switchLayer (target_layer_tab) {
+    var tabs = document.getElementById('layer-select');
+
+    toggleActive(tabs, target_layer_tab);
+}
 
 document.getElementById('files').addEventListener('change', function (e) {
     var file = e.target.files[0];
@@ -12,4 +39,18 @@ document.getElementById('files').addEventListener('change', function (e) {
     };
 
     reader.readAsDataURL(file);
+});
+
+document.getElementById('layer-select').addEventListener('click', function (e) {
+    var target = e.target;
+
+    if (target && target.id === "create-layer") {
+        return createLayer(target);
+    }
+
+    if (target && target.tagName === "LI") {
+        return switchLayer(target);
+    }
+
+    return true;
 });
