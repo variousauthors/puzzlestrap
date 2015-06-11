f63634a68360eec6f0f7071df237bd52497fbd76
Zeigfreid <various.authors@gmail.com>
2015-06-10 21:45:12 -0700

    switching between tabs

--- content/js/file.js
+++ content/js/file.js
@@ -1,4 +1,13 @@
-function toggleActive(context, element) {
+var LAYER_ID = 1;
+var SUFFIX = Math.random().toString().substring(2);
+function layerId () {
+    var id = LAYER_ID;
+    LAYER_ID++;
+
+    return id;
+}
+
+function toggleActive (context, element) {
     var current = context.getElementsByClassName("active"), i;
 
     for (i = 0; i < current.length; i++) {
@@ -9,21 +18,43 @@ function toggleActive(context, element) {
 }
 
 function createLayer (create_layer_tab) {
+    var layer_id = layerId();
+    var name = "Layer " + layer_id;
+    var formal_name = "layer-" + layer_id + "-" + SUFFIX;
+    var last_tab = document.querySelector('#layer-select li:last-child');
     var tabs = document.getElementById('layer-select');
-    var template = document.getElementById("layer-template").innerHTML.trim();
+    var layers = document.getElementById('layers');
+
+    var tab_template = document.getElementById("layer-tab-template").innerHTML.trim();
+    var layer_template = document.getElementById("layer-template").innerHTML.trim();
+    var new_layer_tab = document.createElement('div');
     var new_layer = document.createElement('div');
 
-    new_layer.innerHTML = template;
-    new_layer = new_layer.firstChild;
+    new_layer_tab.innerHTML = tab_template;
+    new_layer_tab = new_layer_tab.firstChild;
+    new_layer_tab.dataset.layerName = formal_name;
+    new_layer_tab.textContent = name;
 
-    tabs.insertBefore(new_layer, create_layer_tab);
-    toggleActive(tabs, new_layer);
+    new_layer.innerHTML = layer_template;
+    new_layer = new_layer.firstChild;
+    new_layer.dataset.layerName = formal_name;
+    new_layer.getElementsByClassName("name")[0].textContent = name;
+
+    tabs.insertBefore(new_layer_tab, last_tab);
+    layers.appendChild(new_layer);
+
+    toggleActive(tabs, new_layer_tab);
+    toggleActive(layers, new_layer);
 }
 
 function switchLayer (target_layer_tab) {
     var tabs = document.getElementById('layer-select');
+    var layers = document.getElementById('layers');
+    var formal_name = target_layer_tab.dataset.layerName;
+    var target_layer = layers.querySelector("[data-layer-name=" + formal_name + "]");
 
     toggleActive(tabs, target_layer_tab);
+    toggleActive(layers, target_layer);
 }
 
 document.getElementById('files').addEventListener('change', function (e) {
--- content/js/main.js
+++ content/js/main.js
@@ -1,3 +1,11 @@
+/* @broofa's simple little uuid http://stackoverflow.com/a/2117523/29182 */
+function getUUID () {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+    });
+}
+
 var AugmentedImageData = function AugmentedImageData (ctx, x, y, w, h) {
     var dataObject, CHUNK_SIZE = 4;
 
